b2db9a1552894eed29247d61a21d8fa5e48bba0e
tuanhuypham <huy5153@gmail.com>
2024-05-29 08:02:40 +0700

    conect database

--- shoper/shoper/Controllers/HomeController.cs
+++ shoper/shoper/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using shoper.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using shoper.Models;
 
 namespace shoper.Controllers
 {
     public class HomeController : Controller
     {
-        private ShoperEntitiesdb db = new ShoperEntitiesdb();
+        readonly Model1 db;
 
+        public HomeController()
+        {
+
+        }
         public ActionResult Index()
         {
             return View();
@@ -29,28 +33,13 @@ namespace shoper.Controllers
 
             return View();
         }
-        [HttpGet]
+       
         public ActionResult Login()
         {
             ViewBag.Message = "Your contact page.";
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Register(Customer customer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Customers.Add(customer);
-                db.SaveChanges();
-                return RedirectToAction("Success");
-            }
-            return View("Login", customer);
-        }
-
-        public ActionResult Success()
-        {
-            return View();
-        }
+        
     }
 }
